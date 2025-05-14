1c5e90846ba48786ee71e1f2a4b0cce128897ba7
shshaidullin@progmaticlab.com <shshaidullin@progmaticlab.com>
2025-05-14 21:20:00 +0300

    update docker-compose and update readme

---
+++ backend/auth.py
@@ -0,0 +1,19 @@
+# список городов
+SITY = ["Moscow", "Novosibirsk", "Kazan"]
+
+# данные для входа на площадку
+VALID_CREDENTIALS = {
+    "Moscow": {"password": "mos123", "label": "Москва"},
+    "Novosibirsk": {"password": "nov456", "label": "Новосибирск"},
+    "Kazan": {"password": "kaz789", "label": "Казань"},
+}
+
+# кодовые слова для заполнения пропусков
+CITY_CODEWORDS = {
+    "Moscow": "тюлень",
+    "Novosibirsk": "новосибирск",
+    "Kazan": "рыба"
+}
+
+# токен Яндекс Диск API
+YANDEX_TOKEN=""
